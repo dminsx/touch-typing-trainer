e6b61de88105c6dbc4311042fa3f1a243f9b3a5e
dminsx <idminsx@gmail.com>
2026-06-02 13:43:47 +0300

    Счетчики, старт, стоп, результат

--- script.js
+++ script.js
@@ -1,11 +1,78 @@
 const board = document.querySelector(".board");
+const time = document.getElementById("timer");
+const speed = document.getElementById("speed");
+const mistakes = document.getElementById("mistakes");
+const accuracy = document.getElementById("accuracy");
 
 const TEXT =
   "В древности библиотеки представляли собой хранилища глиняных табличек и папирусных свитков, доступ к которым имели лишь избранные. Сегодня развитие цифровых технологий и интернета делает информацию доступной каждому человеку на планете всего в несколько кликов.";
-const invalidKeys = ["Shift", "Meta", "Control", "Alt", "GroupNext"];
+const invalidKeys = ["Shift", "Meta", "Control", "Alt", "GroupNext", "Enter"];
 
 let textIndex = 0;
 let textWrong = false;
+let isEnd = false;
+let text = document.querySelectorAll(".text");
+let start = document.getElementById("start");
+let end = document.getElementById("end");
+let mistakesCount = 0;
+let timerId = null;
+let msecValue = 0;
+let correctLetters = 0;
+
+function timer() {
+  msecValue += 100;
+
+  let m = Math.floor(msecValue / 60000) % 60;
+  let s = Math.floor(msecValue / 1000) % 60;
+  let ms = msecValue / 100;
+
+  time.textContent = `${m}:${String(s).padStart(2, 0)}.${String(ms).slice(-1)}`;
+}
+
+function startTimer() {
+  timerId = setInterval(timer, 100);
+}
+
+function endTimer() {
+  clearInterval(timerId);
+}
+
+function resetTimer() {
+  timerId = null;
+  msecValue = 0;
+  time.textContent = `0:00.0`;
+}
+
+function getTime() {
+  return time.textContent;
+}
+
+function getAccuracy() {
+  return (100 - (mistakesCount / TEXT.length) * 100).toFixed(2);
+}
+
+function getSpeed() {
+  return (correctLetters / (msecValue / 60000)).toFixed(0);
+}
+
+function getResult() {
+  alert(`ВАШ РЕЗУЛЬТАТ
+                Время: ${time.textContent}
+                Скорость: ${speed.textContent}
+                Обшибки: ${mistakes.textContent}
+                Точность: ${accuracy.textContent}`);
+}
+
+function confirmFinish() {
+  let finish = confirm("Уверены, что хотите закончить?");
+  endTimer();
+  if (finish) {
+    getResult();
+    resetTrainer();
+  } else {
+    startTimer();
+  }
+}
 
 function createSpan() {
   const span = document.createElement("span");
@@ -14,21 +81,72 @@ function createSpan() {
   return span;
 }
 
-Array.from(TEXT).forEach((letter) => {
-  createSpan().append(letter);
-});
+function initTrainer() {
+  start.removeEventListener("click", initTrainer);
 
-const text = document.querySelectorAll(".text");
+  board.textContent = "3";
+  setTimeout(() => {
+    board.textContent = "2";
+    setTimeout(() => {
+      board.textContent = "1";
+      setTimeout(() => {
+        board.textContent = "Погнали!";
+        setTimeout(() => {
+          board.textContent = "";
+          board.classList.remove("board--start");
+          board.classList.add("board--text");
 
-text[textIndex].classList.add("text--current");
+          Array.from(TEXT).forEach((letter) => {
+            createSpan().append(letter);
+          });
 
-document.addEventListener("keydown", (event) => {
-  if (invalidKeys.includes(event.key)) {
+          text = document.querySelectorAll(".text");
+          text[textIndex].classList.add("text--current");
+          document.addEventListener("keydown", handleKeydown);
+
+          start.textContent = "Закончить";
+          start.id = "end";
+          end = document.getElementById("end");
+          end.addEventListener("click", confirmFinish);
+          startTimer();
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}
+
+function resetTrainer() {
+  resetTimer();
+
+  end.removeEventListener("click", confirmFinish);
+  end.removeEventListener("click", resetTrainer);
+  end.textContent = "Начать";
+  end.id = "start";
+  start = document.getElementById("start");
+  start.addEventListener("click", initTrainer);
+
+  board.textContent = 'Здесь будет текст. Жми "Начать"';
+  board.classList.remove("board--text");
+  board.classList.add("board--start");
+
+  msecValue = 0;
+  textIndex = 0;
+  textWrong = false;
+  isEnd = false;
+  mistakesCount = 0;
+  correctLetters = 0;
+  speed.textContent = `0 зн/мин`;
+  mistakes.textContent = mistakesCount;
+  accuracy.textContent = `${getAccuracy()}%`;
+
+  document.removeEventListener("keydown", handleKeydown);
+}
+
+function handleKeydown(event) {
+  event.preventDefault();
+
+  if (invalidKeys.includes(event.key) || isEnd) {
     return;
-  }
-
-  if (event.key === " ") {
-    event.preventDefault();
   }
 
   if (textWrong) {
@@ -42,14 +160,33 @@ document.addEventListener("keydown", (event) => {
   }
 
   if (event.key === TEXT[textIndex]) {
-    text[textIndex + 1].classList.add("text--current");
-    text[textIndex].classList.remove("text--current");
-    text[textIndex].classList.add("text--correct");
-    textIndex++;
+    if (textIndex < TEXT.length - 1) {
+      text[textIndex + 1].classList.add("text--current");
+      text[textIndex].classList.remove("text--current");
+      text[textIndex].classList.add("text--correct");
+      textIndex++;
+      correctLetters++;
+      speed.textContent = `${getSpeed()} зн/мин`;
+    } else {
+      text[textIndex].classList.remove("text--current");
+      text[textIndex].classList.add("text--correct");
+      correctLetters++;
+      speed.textContent = `${getSpeed()} зн/мин`;
+      isEnd = true;
+      endTimer();
+      setTimeout(() => {
+        getResult();
+        resetTrainer();
+      }, 333);
+    }
   } else if (event.key !== "Backspace") {
     text[textIndex].classList.remove("text--current");
     text[textIndex].classList.add("text--wrong");
     textWrong = true;
-    return;
+    mistakesCount++;
+    mistakes.textContent = mistakesCount;
+    accuracy.textContent = `${getAccuracy()}%`;
   }
-});
+}
+
+start.addEventListener("click", initTrainer);
